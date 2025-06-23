159351267e33c144dc411ef0204099e1488086f0
jejung <jejung@jejungui-MacBookAir.local>
2025-06-24 08:15:14 +0900

    블로그 추가

---
+++ app/blog/page.js
@@ -0,0 +1,19 @@
+'use client';
+
+export default function AboutPage() {
+  return (
+    <>
+    <header>
+        <h1>Custom Blog</h1>
+        <button>새 글 작성</button>
+    </header>
+    <main>
+        <ul>
+            <li>첫 번째 블로그 글</li>
+            <li>두 번째 블로그 글</li>
+            <li>세 번째 블로그 글</li>
+        </ul>
+    </main>
+    </>
+  );
+}
