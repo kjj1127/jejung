8138f857331f2d0da4fa18df6f943190a9599588
jejung <jejung@jejungui-MacBookAir.local>
2025-06-29 16:12:10 +0900

    맥에서 한 작업 옮기기

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,19 +1,71 @@
 'use client';
+import './blog.css';
+import { useState } from 'react';
 
-export default function AboutPage() {
-  return (
-    <>
-    <header>
-        <h1>Custom Blog</h1>
-        <button>새 글 작성</button>
-    </header>
-    <main>
-        <ul>
-            <li>첫 번째 블로그 글</li>
-            <li>두 번째 블로그 글</li>
-            <li>세 번째 블로그 글</li>
-        </ul>
-    </main>
-    </>
-  );
+function App() {
+
+    let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집', '파이썬 독학']);
+    let [따봉, 따봉변경] = useState(new Array(글제목.length).fill(0));
+    console.log(따봉);
+    let new따봉 = [...따봉];
+    console.log(따봉);
+    
+    
+    // 모달 현재 상태 (default)
+    let [modal, setModal] = useState(false);
+
+
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>jejung blog</h4>
+            </div>
+
+            {/* <div className="list">
+                <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍{따봉}</span></h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{글제목[1]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
+                <p>2월 17일 발행</p>
+            </div> */}
+
+            {
+                글제목.map(function(a, i) {
+                    return (
+                        <div className="list" key={i}>
+                            <div className="title-area">
+                                <h4 onClick={() => { setModal(!modal) }}>{글제목[i]}</h4><span onClick={() => { new따봉[i] = new따봉[i] + 1; 따봉변경(new따봉) }}>👍{따봉[i]}</span>
+                            </div>
+                            <p>2월 17일 발행</p>
+                        </div>
+                    )
+                })
+            }
+            {
+                // 조건식 ? 참일때 실행 할 코드 : 거짓일 때 실행할 코드
+                // 1 == 1 ? '맞음' : '아님" -> 맞음
+                // 1 == 2 ? '맞음' : '아님" -> 아님
+                modal == true ? <Modal 글제목 = {[글제목[1]]}/> : null
+                //setModal(modal == false ? true : false)
+            }
+        </div>
+    )
 }
+
+function Modal(props){
+    return(
+        <div className="modal">
+            <h4>{props.글제목}</h4>
+            <p>내용</p>
+            <p>상세내용</p>
+        </div>
+    )
+}
+
+export default App;
